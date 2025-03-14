48c0166b05a522bf5a782daff458bf31532cb879
Léo Chappart <lchappart>
2025-03-14 10:54:22 +0100

    Add Create User

--- _partials/navbar.php
+++ _partials/navbar.php
@@ -1,5 +1,5 @@
 <nav>
-    <img src="../assets/images/AR+FACADES.png" alt="Logo">
+    <img src="assets/images/AR+FACADES.png" alt="Logo">
     <a href="index.php?component=previsionnel">+ Créer un prévisionnel</a>
     <a href="index.php?component=users">Liste des utilisateurs</a>
     <a href="index.php?component=products">Liste des produits</a>
