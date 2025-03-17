217962c55efea04ec7ffc9b2bc4737c01c43d194
lchappart <leo.chappart@coda-student.scchool>
2025-03-17 10:48:19 +0100

    backend listing

--- Controller/products.php
+++ Controller/products.php
@@ -1,4 +1,15 @@
 <?php
 require "Model/products.php";
 
+if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+
+    header('Content-Type: application/json');
+    if (isset($_GET["action"]) && $_GET["action"] == "index") {
+        $page = isset($_GET["page"]) ? $_GET["page"] : 1;
+        $products = getProducts($pdo, $page);
+        echo json_encode($products);
+        exit();
+    }
+}
+
 require "View/products.php";
