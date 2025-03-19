62002c6a5ac07593e3ab9bfa26bfe094b96c419d
lchappart <leo.chappart@coda-student.scchool>
2025-03-19 11:56:22 +0100

    fin du prevu

--- View/previ.php
+++ View/previ.php
@@ -7,7 +7,7 @@
         <label for="totalCost">Coût Total (HT)</label>
         <input type="text" name="totalCost" id="totalCost">
     </div>
-    <div class="section">
+    <section>
         <div class="section-header">
             <h2>Gestion des produits</h2>
             <button id="addRowButton" class="button">+</button>
@@ -24,10 +24,40 @@
                 <th>Actions</th>
             </tr>
             </thead>
-            <tbody>
+            <tbody id="product-tbody">
             </tbody>
         </table>
-    </div>
+    </section>
+    <section>
+        <div class="section-header">
+            <h2>Gestion des équipes</h2>
+        </div>
+        <div class="section-content">
+            <label for="prevision-input"></label>
+            <input id="prevision-input" type="text">
+            <label for="labor-cost"></label>
+            <input id="labor-cost" type="text">
+            <button>Valider</button>
+        </div>
+    </section>
+    <section>
+        <div class="section-header">
+            <h2>Gestions des divers</h2>
+            <button id="add-divers-row">+</button>
+        </div>
+        <div class="section-content">
+            <table id="divers-table">
+                <thead>
+                <tr>
+                    <th>Nom</th>
+                    <th>Coût</th>
+                </tr>
+                </thead>
+                <tbody id="divers-tbody">
+                </tbody>
+            </table>
+        </div>
+    </section>
 </div>
 <script type="module" src="./assets/javascript/services/previ.js"></script>
 <script type="module" src="./assets/javascript/components/previ.js"></script>
@@ -37,12 +67,14 @@
     document.addEventListener("DOMContentLoaded" , async () => {
         const addRow = document.querySelector("#addRowButton")
         const productTable = document.querySelector("#productTable")
-        const tbody = productTable.querySelector("tbody")
+        const productTbody = productTable.querySelector("#product-tbody")
+        const diversTbody = document.querySelector("#divers-tbody")
+        const addDiversRow = document.querySelector("#add-divers-row")
         const products = await getProducts()
         addRow.addEventListener("click", () => {
             let maxId = 1
-            if(tbody.querySelector("tr:last-child")) {
-                maxId = tbody.querySelector("tr:last-child").getAttribute("data-id") + 1
+            if(productTbody.querySelector("tr:last-child")) {
+                maxId = productTbody.querySelector("tr:last-child").getAttribute("data-id") + 1
             }
             const tr = document.createElement("tr")
             tr.setAttribute("data-id", maxId)
@@ -71,7 +103,6 @@
             const typeSelect = tr.querySelector(".type-select")
             const deleteLineButton = tr.querySelector(".delete-line-button")
             tr.querySelector(".select-product").addEventListener("change", (e) => {
-                const selectedProduct = e.target.value
                 updateTypeSelect(products, e.target.value, typeSelect)
             })
             typeSelect.addEventListener("change", () => {
@@ -82,7 +113,26 @@
                 const trToRemove = e.target.closest("tr")
                 trToRemove.remove()
             })
-            tbody.appendChild(tr)
+            productTbody.appendChild(tr)
+        })
+
+        addDiversRow.addEventListener("click", () => {
+            let maxId = 1
+            if(productTbody.querySelector("tr:last-child")) {
+                maxId = productTbody.querySelector("tr:last-child").getAttribute("data-id") + 1
+            }
+            const tr = document.createElement("tr")
+            tr.innerHTML = `
+                <td><input type="text" class="divers-name"></td>
+                <td><input type="text" class="divers-cost"></td>
+                <td><button type="button" class="delete-line-button">Supprimer</button></td>
+            `
+            const deleteLineButton = tr.querySelector(".delete-line-button")
+            deleteLineButton.addEventListener('click', (e) => {
+                const trToRemove = e.target.closest("tr")
+                trToRemove.remove()
+            })
+            diversTbody.appendChild(tr)
         })
     })
 </script>
